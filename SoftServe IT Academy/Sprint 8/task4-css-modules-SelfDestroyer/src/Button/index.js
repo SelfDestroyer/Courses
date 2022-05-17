275fd5af38df3e27f132779d86f68733af25835e
Vova Ivanyuk <ivanyuk.7531@gmail.com>
2022-05-17 20:11:21 +0300

    css-midules

---
+++ SoftServe IT Academy/Sprint 8/task4-css-modules-SelfDestroyer/src/Button/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../Button/active.module.css";
+
+function Button() {
+  return <button className={styles.active}>Click Me</button>;
+}
+
+export default Button;
---
+++ SoftServe IT Academy/Sprint 8/task4-css-modules-SelfDestroyer/src/Input/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../Input/active.module.css";
+
+function Input() {
+  return <input type="text" className={styles.active} />;
+}
+
+export default Input;
